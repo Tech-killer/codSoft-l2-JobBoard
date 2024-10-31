bf016b98bd6a6ff3e6fc7830b3a40c9e772d93e0
Harsh sharma <harsh@Harshs-MacBook-Air.local>
2024-10-31 18:43:18 +0530

    first commit

--- job/src/App.jsx
+++ job/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Home from './Home/Home'
+import Home from './components/Home'
 import Jobs from './components/Jobs'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar'
